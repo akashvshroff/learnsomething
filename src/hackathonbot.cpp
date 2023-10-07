98ea7dccac99cb9a2a111c9f809efda50a006432
akashvshroff <akashvshroff@gmail.com>
2023-10-07 13:39:43 -0500

    initial function stubs and header file

--- src/hackathonbot.cpp
+++ src/hackathonbot.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "hackathonbot.h"
+
+HackathonBot::HackathonBot() {};
+
+void HackathonBot::takeAction(float price) {
+    return;
+}
+
+double HackathonBot::getBalance() {
+    return this->balance;
+}
+
+bool HackathonBot::isHolding() {
+    return this->holding;
+}
+
+void HackathonBot::buy() {
+    this->holding = true;
+}
+
+void HackathonBot::sell() {
+    this->holding = false;
+}
--- src/hackathonbot.h
+++ src/hackathonbot.h
@@ -9,14 +9,18 @@
 #define LEARNSOMETHING_HACKATHONBOT_H
 
 class HackathonBot {
-public:
+    public:
     HackathonBot();
     void takeAction(float price);
     double getBalance();
     bool isHolding();
-private:
+    private:
     double balance;
     bool holding;
+    int priceDownDays = 0;
+    int priceUpDays = 0;
+    void buy();
+    void sell();
 };
 
 #endif //LEARNSOMETHING_HACKATHONBOT_H
