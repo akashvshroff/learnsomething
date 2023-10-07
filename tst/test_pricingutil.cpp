bd3a1f812b9b0cb1c0947cc86f5618fa66a36ad3
akashvshroff <akashvshroff@gmail.com>
2023-10-07 12:33:47 -0500

    added pricingUtil functions and unit tests

--- tst/test_pricingutil.cpp
+++ tst/test_pricingutil.cpp
@@ -1,5 +1,21 @@
 #include <gtest/gtest.h>
+#include <pricingutil.h>
 
 TEST(sampleTest, sample) {
     EXPECT_EQ(4, 4);
+}
+
+TEST(testPricingUtil, calculateTheoreticalVal) {
+    PricingUtil pricingUtil = PricingUtil();
+
+    //standard case
+    EXPECT_EQ(10, pricingUtil.calcVal(5, 1.1, 1));
+    EXPECT_EQ(24, pricingUtil.calcVal(4, 2.1, 2));
+
+    //edge case: 0 interest
+    EXPECT_EQ(18, pricingUtil.calcVal(5, 0, 4));
+
+    //edge case: -ve previous price
+    EXPECT_EQ(-40, pricingUtil.calcVal(-5, 1.1, 4));
+
 }
