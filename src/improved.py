176c13304dbe9fba2cc9c2c844b16de7fca585a9
akashvshroff <akashvshroff@gmail.com>
2023-10-08 19:00:57 -0500

    implemented linear time alg

--- src/improved.py
+++ src/improved.py
@@ -1,13 +1,18 @@
 def max_area(height):
     max_area = 0
-    n = len(height)
+    left = 0
+    right = len(height) - 1
 
-    for i in range(n):
-        for j in range(i + 1, n):
-            h1 = height[i]
-            h2 = height[j]
-            width = j - i
-            current_area = min(h1, h2) * width
-            max_area = max(max_area, current_area)
+    while left < right:
+        h1 = height[left]
+        h2 = height[right]
 
+        width = right - left
+        current_area = min(h1, h2) * width
+        max_area = max(max_area, current_area)
+
+        if h1 < h2:
+            left += 1
+        else:
+            right -= 1
     return max_area
